9ea5d90604be420787864572c74aeff3b9d0f884
ahmed BENLARBI <abenlarbi482@gmail.com>
2026-08-01 20:10:15 +0100

    Update and rename wire_out.vhd to tb/wire_out.vhd

---
+++ tb/wire_out.vhd
@@ -0,0 +1,17 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity wire_out is
+Port ( onewire_out_clk100_INIT : in std_logic;
+onewire_out_clk100_Write: in std_logic;
+onewire_out_clk100_Read : in std_logic;
+onewire_out_clk100 : out std_logic
+);
+end wire_out;
+
+architecture Behavioral of wire_out is
+
+begin
+
+
+end Behavioral;
