744ec1828d6b679c5424c3bd6ccf6672ae60da13
ahmed BENLARBI <abenlarbi482@gmail.com>
2026-08-01 19:55:19 +0100

    Update Resynchro.vhd

--- Resynchro.vhd
+++ Resynchro.vhd
@@ -1,34 +1,6 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 31.03.2026 08:59:27
--- Design Name: 
--- Module Name: Resynchro - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity Resynchro is
 Port ( 
@@ -38,9 +10,7 @@ Port (
   debug_in : in std_logic_vector(7 downto 0);
   onewire_in : in std_logic;
   onewire_in_clk : out std_logic;
-  debug_in_clk : out std_logic_vector(7 downto 0)
-  
-  
+  debug_in_clk : out std_logic_vector(7 downto 0) 
   );
 end Resynchro;
 
